61159cc044b4e23d586c898dead98b9103a4f529
Zhewen Hu <hzw.fae.fmh@gmail.com>
2026-05-13 02:58:39 +0000

    backup

--- suite_figure8.sh
+++ suite_figure8.sh
@@ -1,6 +1,7 @@
 python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure8/direct.yaml --output_root_dir paper_experiments/figure8/direct.yaml
 python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure8/learned_projected.yaml --output_root_dir paper_experiments/figure8/learned_projected.yaml
 python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure8/random_projected.yaml --output_root_dir paper_experiments/figure8/random_projected.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure0/figure0_clean.yaml --output_root_dir paper_experiments/figure0/figure0_clean.yaml
 
 
 
