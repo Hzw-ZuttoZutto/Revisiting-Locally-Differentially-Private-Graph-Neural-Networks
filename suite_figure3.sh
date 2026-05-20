2d7cc2ab49859764b7848dabc6e2554834eaea78
Zhewen Hu <hzw.fae.fmh@gmail.com>
2026-05-20 03:58:50 +0000

    backup

--- suite_figure3.sh
+++ suite_figure3.sh
@@ -1,3 +1,12 @@
-python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/figure3_pipeline1.yaml --output_root_dir paper_experiments/figure3/figure3_pipeline1.yaml
-python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/figure3_pipeline2.yaml --output_root_dir paper_experiments/figure3/figure3_pipeline2.yaml
-python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/figure3_pipeline3.yaml --output_root_dir paper_experiments/figure3/figure3_pipeline3.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/sage/figure3_pipeline1.yaml --output_root_dir paper_experiments/figure3/sage/figure3_pipeline1.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/sage/figure3_pipeline2.yaml --output_root_dir paper_experiments/figure3/sage/figure3_pipeline2.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/sage/figure3_pipeline3.yaml --output_root_dir paper_experiments/figure3/sage/figure3_pipeline3.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/sage/figure3_pipeline4.yaml --output_root_dir paper_experiments/figure3/sage/figure3_pipeline4.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gcn/figure3_pipeline1.yaml --output_root_dir paper_experiments/figure3/gcn/figure3_pipeline1.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gcn/figure3_pipeline2.yaml --output_root_dir paper_experiments/figure3/gcn/figure3_pipeline2.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gcn/figure3_pipeline3.yaml --output_root_dir paper_experiments/figure3/gcn/figure3_pipeline3.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gcn/figure3_pipeline4.yaml --output_root_dir paper_experiments/figure3/gcn/figure3_pipeline4.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gat/figure3_pipeline1.yaml --output_root_dir paper_experiments/figure3/gat/figure3_pipeline1.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gat/figure3_pipeline2.yaml --output_root_dir paper_experiments/figure3/gat/figure3_pipeline2.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gat/figure3_pipeline3.yaml --output_root_dir paper_experiments/figure3/gat/figure3_pipeline3.yaml
+python -m hparams_search_scripts.run_mechanism_hparam_search --config configs_final/figure3/gat/figure3_pipeline4.yaml --output_root_dir paper_experiments/figure3/gat/figure3_pipeline4.yaml
